6d3bdc3f78f3456aef6a6d6eaf9bc9348641f727
Zander2895 <jacquesalexander71@gmail.com>
2024-07-02 15:14:12 -0400

    first commit

---
+++ product_review_analysis.py
@@ -0,0 +1,14 @@
+reviews = [
+        "This product is really good. I'm impressed with its quality.",
+        "The performance of this product is excellent. Highly recommended!",
+        "I had a bad experience with this product. It didn't meet my expectations.",
+        "Poor quality product. Wouldn't recommend it to anyone.",
+        "The product was average. Nothing extraordinary about it."
+    ]
+
+keywords = ["good", "excellent", "bad", "poor", "average"]
+
+def highlighted(reviews, keywords):
+    highlighted = []
+    for review in reviews:
+        for keyword in keywords:
