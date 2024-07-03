08458c8fbfcbb5f8d8bd471bf37472144ec58f87
Zander2895 <jacquesalexander71@gmail.com>
2024-07-02 22:34:55 -0400

    new commit

--- product_review_analysis.py
+++ product_review_analysis.py
@@ -1,3 +1,4 @@
+#Frist exercise
 reviews = [
         "This product is really good. I'm impressed with its quality.",
         "The performance of this product is excellent. Highly recommended!",
@@ -12,3 +13,29 @@ def highlighted(reviews, keywords):
     highlighted = []
     for review in reviews:
         for keyword in keywords:
+            if keyword in review:
+                print(review.replace(keyword, keyword.upper()))
+            elif keyword.capitalize() in review:
+                print(review.replace(keyword.capitalize(), keyword.upper()))
+highlighted(reviews, keywords)   
+
+#Second exercise
+positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"]
+
+negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
+
+def tally(positive_words, negative_words, reviews):
+    tally = []
+    positive_counter = 0
+    negative_counter = 0
+    for review in reviews:
+        for positive_word in positive_words:
+            if positive_word in review:
+                positive_counter += 1
+        for negative_word in negative_words:
+            if negative_word in review:
+                negative_counter -= 1
+                
+# count()
+#startwith()
+#find()
